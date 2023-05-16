251d6c91c19ad688ab6a71db6c4da155a85a54b7
Unai Fifo <unaigarciar@gmail.com>
2023-05-16 15:21:07 +0200

    Change ex1 to collections

--- javaTests/day9/src/main/java/org/example/Main.java
+++ javaTests/day9/src/main/java/org/example/Main.java
@@ -1,18 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int noMatches = Integer.parseInt(scanner.nextLine());
-        int[] runs = new int[noMatches];
+        List<Integer> runs = new ArrayList<>();
         for(int index=0;index<noMatches;index++){
-            runs[index] =Integer.parseInt(scanner.nextLine());
+            runs.add(Integer.parseInt(scanner.nextLine()));
         }
         int total = 0;
-        for (int i = 0; i < runs.length; i++) {
-            total += runs[i];
+        for (int run : runs) {
+            total += run;
         }
         System.out.println();
         System.out.println(total);
